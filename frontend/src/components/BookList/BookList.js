114132537959801399594887230d8377b402e106
Anton <tonyeasytrade@gmail.com>
2024-04-09 10:17:48 +0300

    Filter redux slice and filter by title

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -2,12 +2,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs'
 import './BookList.css';
+import { selectTitleFilter } from '../../redux/slices/filterSlice';
 
 
 const BookList = () => {
-    const books = useSelector((state) => state.books)
-    const dispatch = useDispatch()
 
+    const books = useSelector((state) => state.books)
+    const titleFilter = useSelector(selectTitleFilter)
+    const dispatch = useDispatch()
 
     const handleDeleteBook = (id) => {
         dispatch(deleteBook(id))
@@ -17,11 +19,16 @@ const BookList = () => {
         dispatch(toggleFavorite(id))
     }
 
+    const fileredBook = books.filter((book) => {
+        const matchesTitle = book.title.toLowerCase().includes(titleFilter.toLowerCase())
+        return matchesTitle
+    })
+
     return (
         <div className='app-block book-list'>
             <h2>Book List</h2>
             {books.length === 0 ? (<p>No books available</p>) : (<ul>
-                {books.map((book, i) => (
+                {fileredBook.map((book, i) => (
                     <li key={book.id}>
                         <div className='book-info'>{++i}. {book.title} by <strong>{book.author}</strong></div>
                         <div className='book-actions'>
